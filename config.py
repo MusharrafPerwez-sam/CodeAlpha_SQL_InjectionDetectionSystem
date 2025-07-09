be68019ce0f9a28ac83a4c0ddd5916de7ac80f5b
MusharrafPerwez-sam <musharrafperwez0512@gmail.com>
2025-07-10 01:41:46 +0530

    Fix: AES key size issue and updated Flask app code

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SECRET_KEY = 'your_flask_secret_key'
-ENCRYPTION_KEY = b'your_32_byte_key_here_123456789012'  # Must be 32 bytes for AES-256
+SECRET_KEY = 'your_secret_key'  # can be any random string
+ENCRYPTION_KEY = b'ThisIsA32ByteLongEncryptionKey!!'  # 32 bytes key for AES
